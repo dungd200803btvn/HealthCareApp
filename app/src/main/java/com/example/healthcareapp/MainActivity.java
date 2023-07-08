752ebfe957fe5fe22838191d3b15b920674fa001
Lê Chí Dũng <dung.lc@sis.hust.edu.vn>
2023-07-08 18:51:10 +0700

    fix the width size of list view

--- app/src/main/java/com/example/healthcareapp/MainActivity.java
+++ app/src/main/java/com/example/healthcareapp/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword("ledung200803@gmail.com","dung2003");
     }
 }
